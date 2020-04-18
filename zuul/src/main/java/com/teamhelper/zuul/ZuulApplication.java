5ee457d53dc0fcbd9d074bf059f74a191d4757ab
dduck00 <ejr075@gmail.com>
2020-04-19 01:13:09 +0900

    zuul2.0 -> spring cloud gateway 변경

--- zuul/src/main/java/com/teamhelper/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/teamhelper/zuul/ZuulApplication.java
@@ -3,15 +3,22 @@ package com.teamhelper.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
-@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder){
+        return builder.routes().build();
     }
 
 }
