e4994a5f22b03876edf4f03b1cbabed2c651e03b
dduck00 <ejr075@gmail.com>
2020-04-21 00:03:50 +0900

    롤백..

--- chat_service/src/main/java/com/teamhelper/chatservice/controller/ChatController.java
+++ chat_service/src/main/java/com/teamhelper/chatservice/controller/ChatController.java
@@ -1,38 +1,26 @@
 package com.teamhelper.chatservice.controller;
 
 import com.teamhelper.chatservice.dto.ChatMessage;
-import com.teamhelper.chatservice.feign.ViewFeign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.util.HtmlUtils;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/chat/")
 public class ChatController {
 
     private final SimpMessageSendingOperations messagingTemplate;
-    private final ViewFeign viewFeign;
 
     @Autowired
-    public ChatController(SimpMessageSendingOperations messagingTemplate, ViewFeign viewFeign){
+    public ChatController(SimpMessageSendingOperations messagingTemplate ){
         this.messagingTemplate = messagingTemplate;
-        this.viewFeign = viewFeign;
     }
 
-    @GetMapping("/")
-    public ModelAndView mainPage(){
-        return viewFeign.chatView();
-    }
 
     @MessageMapping("/message")
     public void message(ChatMessage message) {
