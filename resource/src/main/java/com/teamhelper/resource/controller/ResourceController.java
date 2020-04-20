59d85abe21a80f8bf2b921940d3a7fd5176547fc
dduck00 <ejr075@gmail.com>
2020-04-20 22:51:12 +0900

    static 리소스 & webjars 리소스 라우팅

---
+++ resource/src/main/java/com/teamhelper/resource/controller/ResourceController.java
@@ -0,0 +1,8 @@
+package com.teamhelper.resource.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+}
--- resource/src/main/resources/static/js/app.js
+++ resource/src/main/resources/static/js/app.js
@@ -12,12 +12,12 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/gs-guide-websocket');
+    var socket = new SockJS('/chat/gs-guide-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
+        stompClient.subscribe('/chat/topic/greetings', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
     });
@@ -32,7 +32,7 @@ function disconnect() {
 }
 
 function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+    stompClient.send("/chat/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
 }
 
 function showGreeting(message) {
