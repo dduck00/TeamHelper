26554b713a56be3edc44ff32e770157ca117c6a0
dduck00 <ejr075@gmail.com>
2020-04-22 23:41:42 +0900

    CORS문제 해결... WebSocket을 최초생성할 때 8080으로 생성할 경우, iframe에 8080으로 묶이는 모양이다.

--- zuul/src/main/java/com/teamhelper/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/teamhelper/zuul/ZuulApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +23,8 @@ public class ZuulApplication {
     private final String[] webSocketUris = {
             "/ws/connect/**",
             "/sub/group/**",
-            "/pub/get/msg/**"
+            "/pub/get/msg/**",
+            "/ws"
     };
 
 
