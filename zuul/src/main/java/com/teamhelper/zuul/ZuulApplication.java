cde4c237c20e3c4acc9334c8ea2cdf591aadb986
dduck00 <ejr075@gmail.com>
2020-05-06 23:21:53 +0900

    Gateway Predicate 조건 추가 Jwt토큰 유효성 검사

--- zuul/src/main/java/com/teamhelper/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/teamhelper/zuul/ZuulApplication.java
@@ -1,5 +1,7 @@
 package com.teamhelper.zuul;
 
+import com.teamhelper.zuul.jwt.JwtToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +9,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -25,27 +29,42 @@ public class ZuulApplication {
             "/pub/get/msg/**"
     };
 
+    private final JwtToken JWT_TOKEN;
+
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
 
+    @Autowired
+    ZuulApplication(JwtToken jwtToken) {
+        this.JWT_TOKEN = jwtToken;
+    }
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-//                .route(p -> p
-//                        .path("/**")
-//                        .filters(f -> f.rewritePath("^\\/[0-9a-zA-Z\\/]*$", "/login"))
-//                        .uri("lb://AUTH-SERVICE")
-//                        .predicate(pre -> pre.getRequest().getCookies().isEmpty()))
+                .route(p -> p
+                        .path("/**")
+                        .filters(f -> f.rewritePath("^\\/[0-9a-zA-Z\\/]*$", "/login"))
+                        .uri("http://naver.com")
+                        .predicate(pre -> {
+
+                            Map<String, String> token = JWT_TOKEN.parseHeader(pre.getRequest());
+
+                            if (token == null) {
+                                return true;
+                            }
+                            pre.getRequest().getHeaders().setAll(token);
+                            return false;
+                        }))
                 .route(p -> p
                         .path("/api/auth/**")
                         .uri("lb://AUTH-SERVICE"))
                 .route(p -> p
                         .path(webSocketUris)
                         .uri("lb://WS-SERVICE"))
-                .route(p ->p
+                .route(p -> p
                         .path(resourceUris)
                         .uri("lb://RESOURCE-SERVICE"))
                 .build();
---
+++ zuul/src/main/java/com/teamhelper/zuul/jwt/JwtToken.java
@@ -0,0 +1,59 @@
+package com.teamhelper.zuul.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.eclipse.jgit.util.StringUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class JwtToken {
+
+    private final String KEY = "DUCK";
+    private final String SECRET_KEY;
+    private final long TokenValidTime = 30 * 60 * 100L;
+
+    JwtToken() {
+        SECRET_KEY = Base64.getEncoder().encodeToString(KEY.getBytes());
+        System.out.println(SECRET_KEY);
+    }
+
+    public Map<String, String> parseHeader(ServerHttpRequest request) {
+        String token = resolveToken(request);
+
+        if (StringUtils.isEmptyOrNull(token)
+                || validateToken(token) == false) {
+            return null;
+        }
+        Map<String, String> user = new HashMap<>();
+
+        System.out.println(token);
+        String[] userInfos = getUserPk(token).split("/");
+
+        user.put("USER_CODE", userInfos[0]);
+        user.put("ID", userInfos[1]);
+
+        return user;
+    }
+
+    private String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    private String resolveToken(ServerHttpRequest request) {
+        List<String> header = request.getHeaders().get("X-AUTH-TOKEN");
+        if (header == null)
+            return null;
+        return header.get(0);
+    }
+
+    private boolean validateToken(String token) {
+        Jws<Claims> claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+        return !claims.getBody().getExpiration().before(new Date());
+    }
+
+
+}
