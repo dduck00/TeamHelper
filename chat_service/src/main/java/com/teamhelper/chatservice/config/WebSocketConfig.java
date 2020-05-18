a09eab5426496f55190f1eba528da7f5d2dcd93a
dduck00 <ejr075@gmail.com>
2020-05-18 22:43:40 +0900

    할일 요약

--- chat_service/src/main/java/com/teamhelper/chatservice/config/WebSocketConfig.java
+++ chat_service/src/main/java/com/teamhelper/chatservice/config/WebSocketConfig.java
@@ -19,5 +19,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/connect/").setAllowedOrigins("*").withSockJS().setClientLibraryUrl("/webjars/sockjs-client/1.0.2/sockjs.min.js");
+        /*
+        TODO : 검증프로세스 추가 -> 1. 유저가 정당한가        2. 유저가 그룹의 멤버가 맞는가.         3. 둘중 하나라도 아니면 exception throw
+
+         */
     }
 }
